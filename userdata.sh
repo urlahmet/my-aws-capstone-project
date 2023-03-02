40d0d7dc6d3e7752aa131fb71bab93257ae42ebb
Ahmet <leonidazarov5@gmail.com>
2023-03-02 12:59:34 +0300

    capstone proje eklendi

--- userdata.sh
+++ userdata.sh
@@ -4,7 +4,7 @@ apt-get install git -y
 apt-get install python3 -y
 cd /home/ubuntu/
 TOKEN="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"
-git clone https://$TOKEN@github.com/urlahmet/my-aws-capstone-project
+git clone https://$TOKEN@github.com/urlahmet/my-aws-capstone-project.git
 cd /home/ubuntu/my-aws-capstone-project
 apt install python3-pip -y
 apt-get install python3.7-dev default-libmysqlclient-dev -y
